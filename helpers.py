bbcbef13ac75501ed0f62b26287393f49aba9942
Mohammed Danish <danish4198@gmail.com>
2020-11-07 23:46:16 -0500

    Parallelized some parts.

--- helpers.py
+++ helpers.py
@@ -2,9 +2,14 @@
 
 import musdb
 import random
+import librosa
+import numpy as np
+from sklearn.decomposition import non_negative_factorization
+
+from typing import Tuple
 
 
-def generate_two_stem_data(mus: musdb.DB, chunk_duration=5.0):
+def generate_two_stem_data(mus: musdb.DB, chunk_duration=5.0) -> Tuple[np.ndarray, np.ndarray, np.ndarray, int]:
     """Generator for data to extract 2 stems, vocals and accompaniment, from a mixture.
 
     Parameters
@@ -31,7 +36,8 @@ def generate_two_stem_data(mus: musdb.DB, chunk_duration=5.0):
     """
     track = random.choice(mus.tracks)
     track.chunk_duration = chunk_duration
-    track.chunk_start = random.uniform(0, track.duration - track.chunk_duration)
+    track.chunk_start = random.uniform(
+        0, track.duration - track.chunk_duration)
 
     mixture = track.audio
     vocals = track.targets["vocals"].audio
@@ -39,3 +45,115 @@ def generate_two_stem_data(mus: musdb.DB, chunk_duration=5.0):
     rate = track.rate
 
     yield mixture, vocals, accompaniment, rate
+
+
+def make_mono(audio: np.ndarray) -> np.ndarray:
+    """Get single channel audio data from stereo audio.
+
+    Parameters
+    ----------
+    audio : np.ndarray, shape (n_samples, 2)
+        Stereo audio data.
+
+    Returns
+    -------
+    audio_mono : np.ndarray, shape (n_samples,)
+        Mono audio data formed by averaging channels.
+    """
+    audio_mono = 0.5 * (audio[:, 0] + audio[:, 1])
+    return audio_mono
+
+
+def get_magphase(audio: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """Returns magnitudes and phases of the audio's spectrogram such that ``stft = mags * phases''.
+
+    Parameters
+    ----------
+    audio : np.ndarray, shape (n_samples,)
+        Mono audio data.
+
+    Returns
+    -------
+    audio_mags : np.ndarray, shape (4096, n_frames)
+        Magnitude spectrogram of stft.
+
+    audio_phases : np.ndarray, shape (4096, n_frames)
+        Phase factors of stft.
+    """
+    audio_stft = librosa.stft(audio, n_fft=4096,
+                              hop_length=2048,
+                              win_length=4096,
+                              center=False)
+    audio_mags, audio_phases = librosa.magphase(audio_stft)
+
+    return audio_mags, audio_phases
+
+
+def reconstruct_audio(components: np.ndarray, weights: np.ndarray, phases: np.ndarray) -> np.ndarray:
+    """Reconstructs audio from learned components and weights and the supplied phases.
+
+    Parameters
+    ----------
+    components : np.ndarray, shape (4096, n_atoms)
+        Learned components/atoms/bases from data.
+
+    weights : np.ndarray, shape (n_atoms, n_frames)
+        Learned weights from data.
+
+    Returns
+    -------
+    reconstruction : np.ndarray, shape (n_samples,)
+        Reconstructed audio.
+    """
+    mags = components @ weights
+    stft = mags * phases
+    reconstruction = librosa.istft(stft,
+                                   hop_length=2048,
+                                   win_length=4096,
+                                   center=False)
+    return reconstruction
+
+
+def learn_representation(audio: np.ndarray, n_components: int = 100, max_iter: int = 400):
+    mags, phases = get_magphase(audio)
+    components, weights, n_iters = non_negative_factorization(mags,
+                                                              n_components=n_components,
+                                                              beta_loss="kullback-leibler",
+                                                              solver="mu",
+                                                              max_iter=400)
+    return components, weights, n_iters
+
+
+class NMFResults:
+    vocals: Tuple[np.ndarray, np.ndarray, int]
+    accompaniment: Tuple[np.ndarray, np.ndarray, int]
+    drums: Tuple[np.ndarray, np.ndarray, int]
+    bass: Tuple[np.ndarray, np.ndarray, int]
+    others: Tuple[np.ndarray, np.ndarray, int]
+
+    def __init__(self) -> None:
+        self.vocals = None
+        self.accompaniment = None
+        self.drums = None
+        self.bass = None
+        self.others = None
+
+    def set_vocals(self, result):
+        print("Set Vocals")
+        self.vocals = result
+
+    def set_accompaniment(self, result):
+        print("Set Accompaniment")
+        self.accompaniment = result
+
+    def set_drums(self, result):
+        print("Set Drums")
+        self.drums = result
+
+    def set_bass(self, result):
+        print("Set Bass")
+        self.bass = result
+
+    def set_others(self, result):
+        print("Set Others")
+        self.others = result
--- nmf.py
+++ nmf.py
@@ -3,55 +3,70 @@
 from pathlib import Path
 import numpy as np
 import musdb
-import matplotlib.pyplot as plt
-from sklearn.decomposition import non_negative_factorization
+# import matplotlib.pyplot as plt
 from sklearn.linear_model import Lasso
-import librosa
 from soundfile import write
 import helpers
+from multiprocessing import Pool
+
 
 # TODO: MAKE EVERYTHING A FUNCTION
 if __name__ == "__main__":
     root = Path("..", "musdb18hq")
 
-    mus_train = musdb.DB(root=root, subsets="train", split="train", is_wav=True)
-    mus_valid = musdb.DB(root=root, subsets="train", split="valid", is_wav=True)
+    mus_train = musdb.DB(root=root, subsets="train",
+                         split="train", is_wav=True)
+    mus_valid = musdb.DB(root=root, subsets="train",
+                         split="valid", is_wav=True)
     mus_test = musdb.DB(root=root, subsets="test", is_wav=True)
 
-    mixture, vocals, accompaniment, rate = next(helpers.generate_two_stem_data(mus_train, chunk_duration=5.0))
+    mixture, vocals, accompaniment, rate = next(
+        helpers.generate_two_stem_data(mus_train, chunk_duration=5.0))
 
-    mixture_mono = 0.5 * (mixture[:, 0] + mixture[:, 1])
-    vocals_mono = 0.5 * (vocals[:, 0] + vocals[:, 1])
-    accompaniment_mono = 0.5 * (accompaniment[:, 0] + accompaniment[:, 1])
+    vocals_mono = helpers.make_mono(vocals)
+    accompaniment_mono = helpers.make_mono(accompaniment)
 
-    mixture_stft = librosa.stft(mixture_mono, n_fft=2048, hop_length=256, win_length=2048, center=False)
-    vocals_stft = librosa.stft(vocals_mono, n_fft=2048, hop_length=256, win_length=2048, center=False)
-    accompaniment_stft = librosa.stft(accompaniment_mono, n_fft=2048, hop_length=256, win_length=2048, center=False)
-
-    mixture_mags, mixture_phases = librosa.magphase(mixture_stft)
-    vocals_mags, vocals_phases = librosa.magphase(vocals_stft)
-    accompaniment_mags, accompaniment_phases = librosa.magphase(accompaniment_stft)
-
-    # X = W @ H. W (n_channels, n_atoms), H (n_atoms, n_time_frames)
     # Hyperparameter
-    n_components = 2048
+    n_components = 40
 
-    print("Starting Vocals NMF")
-    vocals_components, vocals_weights, n_iter = non_negative_factorization(vocals_mags, n_components=n_components, beta_loss="kullback-leibler", solver="mu", max_iter=400)
-    print(f"Finished Vocals NMF in {n_iter} iterations.")
+    pool = Pool()
 
-    print("Starting Accomponiment NMF")
-    accompaniment_components, accompaniment_weights, n_iter = non_negative_factorization(accompaniment_mags, n_components=n_components, beta_loss="kullback-leibler", solver="mu", max_iter=400)
-    print(f"Finished Accomponiment NMF in {n_iter} iterations")
+    results = helpers.NMFResults()
 
-    # BV bases for vocals
-    # BA bases for acoompaniment
-    # B = [BV BA]
-    mixture_components = np.hstack((vocals_components, accompaniment_components))
+    print("Starting Vocals")
+    pool.apply_async(helpers.learn_representation,
+                     args=(vocals_mono,),
+                     kwds={
+                         "n_components": n_components,
+                         "max_iter": 400
+                     },
+                     callback=results.set_vocals)
+
+    print("Starting Accompaniment")
+    pool.apply_async(helpers.learn_representation,
+                     args=(accompaniment_mono,),
+                     kwds={
+                         "n_components": n_components,
+                         "max_iter": 400
+                     },
+                     callback=results.set_accompaniment)
+
+    pool.close()
+    pool.join()
+
+    vocals_components, vocals_weights, vocals_iter = results.vocals
+    accompaniment_components, accompaniment_weights, accompaniment_iter = results.accompaniment
+
+    mixture_components = np.hstack(
+        (vocals_components, accompaniment_components))
+
+    mixture_mono = helpers.make_mono(mixture)
+    mixture_mags, mixture_phases = helpers.get_magphase(mixture_mono)
 
     # TODO: Replace with KL Divergence based iterations
     print("Starting Separation with Lasso")
-    model = Lasso(alpha=0.5, fit_intercept=False, positive=True, selection="random")
+    model = Lasso(alpha=0.1, fit_intercept=False,
+                  positive=True, selection="random")
     model.fit(mixture_components, mixture_mags)
     mixture_weights = model.coef_.T
     print("Finished Separation with Lasso")
@@ -59,14 +74,10 @@ if __name__ == "__main__":
     learned_vocals_weights = mixture_weights[:n_components, :]
     learned_accompaniment_weights = mixture_weights[n_components:, :]
 
-    learned_vocals_mags = vocals_components @ learned_vocals_weights
-    learned_accompaniment_mags = accompaniment_components @ learned_accompaniment_weights
-
-    learned_vocals_stft = learned_vocals_mags * mixture_phases
-    learned_accompaniment_stft = learned_accompaniment_mags * mixture_phases
-
-    learned_vocals = librosa.istft(learned_vocals_stft, hop_length=256, win_length=2048, center=False)
-    learned_accompaniment = librosa.istft(learned_accompaniment_stft, hop_length=256, win_length=2048, center=False)
+    learned_vocals = helpers.reconstruct_audio(
+        vocals_components, learned_vocals_weights, mixture_phases)
+    learned_accompaniment = helpers.reconstruct_audio(
+        accompaniment_components, learned_accompaniment_weights, mixture_phases)
 
     write("mixture.wav", mixture, rate)
     write("learned_vocals.wav", learned_vocals, rate)
