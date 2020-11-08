f8329fc7d7bd9835b4b67d35cb00447926e10407
Mohammed Danish <danish4198@gmail.com>
2020-11-08 06:29:07 -0500

    Separate to 4 stems and use OMP for separation

--- helpers.py
+++ helpers.py
@@ -17,8 +17,8 @@ def generate_two_stem_data(mus: musdb.DB, chunk_duration=5.0) -> Tuple[np.ndarra
     mus : musdb.DB
         Generator of musdb dataset.
 
-    chunk_duration : float, default = 5.0
-        Duration of chunks to yield in seconds
+    chunk_duration : float, optional, default = 5.0
+        Duration of chunks to yield in seconds.
 
     Returns
     -------
@@ -47,6 +47,52 @@ def generate_two_stem_data(mus: musdb.DB, chunk_duration=5.0) -> Tuple[np.ndarra
     yield mixture, vocals, accompaniment, rate
 
 
+def generate_four_stem_data(mus: musdb.DB, chunk_duration=5.0) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, int]:
+    """Generator for data to extract 4 stems, vocals, drums, bass, and others, from a mixture.
+
+    Parameters
+    ----------
+    mus : musdb.DB
+        Generator of musdb dataset.
+
+    chunk_duration : float, optional, default = 5.0
+        Duration of chunks to yield in seconds.
+
+    Returns
+    -------
+    mixture : np.ndarray, (nsamples, 2)
+        Mixture data
+
+    vocals : np.ndarray, (nsamples, 2)
+        Vocals data
+
+    drums : np.ndarray, (nsamples, 2)
+        Accompaniment data
+
+    bass : np.ndarray, (nsamples, 2)
+        Accompaniment data
+
+    others : np.ndarray, (nsamples, 2)
+        Accompaniment data
+
+    rate : int
+        Sample rate of audio
+    """
+    track = random.choice(mus.tracks)
+    track.chunk_duration = chunk_duration
+    track.chunk_start = random.uniform(
+        0, track.duration - track.chunk_duration)
+
+    mixture = track.audio
+    vocals = track.targets["vocals"].audio
+    drums = track.targets["drums"].audio
+    bass = track.targets["bass"].audio
+    others = track.targets["other"].audio
+    rate = track.rate
+
+    yield mixture, vocals, drums, bass, others, rate
+
+
 def make_mono(audio: np.ndarray) -> np.ndarray:
     """Get single channel audio data from stereo audio.
 
@@ -64,7 +110,7 @@ def make_mono(audio: np.ndarray) -> np.ndarray:
     return audio_mono
 
 
-def get_magphase(audio: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+def get_magphase(audio: np.ndarray, win_length: int = 1024) -> Tuple[np.ndarray, np.ndarray]:
     """Returns magnitudes and phases of the audio's spectrogram such that ``stft = mags * phases''.
 
     Parameters
@@ -72,33 +118,72 @@ def get_magphase(audio: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     audio : np.ndarray, shape (n_samples,)
         Mono audio data.
 
+    win_length : int, optional, default = 1024
+        Length of Hann window to use.
+
     Returns
     -------
-    audio_mags : np.ndarray, shape (4096, n_frames)
+    audio_mags : np.ndarray, shape (8192, n_frames)
         Magnitude spectrogram of stft.
 
-    audio_phases : np.ndarray, shape (4096, n_frames)
+    audio_phases : np.ndarray, shape (8192, n_frames)
         Phase factors of stft.
     """
-    audio_stft = librosa.stft(audio, n_fft=4096,
-                              hop_length=2048,
-                              win_length=4096,
+    audio_stft = librosa.stft(audio, n_fft=win_length,
+                              hop_length=win_length // 2,
+                              win_length=win_length,
                               center=False)
     audio_mags, audio_phases = librosa.magphase(audio_stft)
 
     return audio_mags, audio_phases
 
 
-def reconstruct_audio(components: np.ndarray, weights: np.ndarray, phases: np.ndarray) -> np.ndarray:
-    """Reconstructs audio from learned components and weights and the supplied phases.
+def get_cqt_magphase(audio: np.ndarray, rate: int = 44100) -> Tuple[np.ndarray, np.ndarray]:
+    """Returns magnitudes and phases of the audio's Constant Q-Transform
+    such that ``cqt = mags * phases''.
 
     Parameters
     ----------
-    components : np.ndarray, shape (4096, n_atoms)
+    audio : np.ndarray, shape (n_samples,)
+        Mono audio data.
+
+    rate : int, optional, default = 44100
+        Sample rate of audio.
+
+    Returns
+    -------
+    audio_mags : np.ndarray, shape (8192, n_frames)
+        Magnitude constant Q-transform of stft.
+
+    audio_phases : np.ndarray, shape (8192, n_frames)
+        Phase factors of cqt.
+    """
+    audio_cqt = librosa.cqt(audio,
+                            sr=rate,
+                            n_bins=252,
+                            bins_per_octave=36)
+    audio_mags, audio_phases = librosa.magphase(audio_cqt)
+
+    return audio_mags, audio_phases
+
+
+def reconstruct_audio(components: np.ndarray, weights: np.ndarray, phases: np.ndarray, win_length: int = 1024) -> np.ndarray:
+    """Reconstructs audio from learned components and weights and the supplied phases
+    using and inverse STFT.
+
+    Parameters
+    ----------
+    components : np.ndarray, shape (8192, n_atoms)
         Learned components/atoms/bases from data.
 
     weights : np.ndarray, shape (n_atoms, n_frames)
         Learned weights from data.
+
+    phases : np.ndarray, shape (8192, n_frames)
+        Phase factors to use for reconstruction.
+
+    win_length : int, optional, default = 1024
+        Length of Hann window used.
 
     Returns
     -------
@@ -108,19 +193,61 @@ def reconstruct_audio(components: np.ndarray, weights: np.ndarray, phases: np.nd
     mags = components @ weights
     stft = mags * phases
     reconstruction = librosa.istft(stft,
-                                   hop_length=2048,
-                                   win_length=4096,
+                                   hop_length=win_length // 2,
+                                   win_length=win_length,
                                    center=False)
     return reconstruction
 
 
-def learn_representation(audio: np.ndarray, n_components: int = 100, max_iter: int = 400):
-    mags, phases = get_magphase(audio)
+def reconstruct_cqt_audio(components: np.ndarray, weights: np.ndarray, phases: np.ndarray, rate: int = 44100) -> np.ndarray:
+    """Reconstructs audio from learned components and weights and the supplied phases
+    using an inverse Constant Q-Transform.
+
+    Parameters
+    ----------
+    components : np.ndarray, shape (8192, n_atoms)
+        Learned components/atoms/bases from data.
+
+    weights : np.ndarray, shape (n_atoms, n_frames)
+        Learned weights from data.
+
+    phases : np.ndarray, shape (8192, n_frames)
+        Phase factors to use for reconstruction.
+
+    rate : int, optional, default = 44100
+        Sample rate of audio.
+
+    Returns
+    -------
+    reconstruction : np.ndarray, shape (n_samples,)
+        Reconstructed audio.
+    """
+    mags = components @ weights
+    cqt = mags * phases
+    reconstruction = librosa.icqt(cqt,
+                                  sr=rate,
+                                  bins_per_octave=36)
+    return reconstruction
+
+
+def learn_representation(audio: np.ndarray, win_length: int = 1024, n_components: int = 100, max_iter: int = 400):
+    mags, phases = get_magphase(audio, win_length=win_length)
     components, weights, n_iters = non_negative_factorization(mags,
                                                               n_components=n_components,
                                                               beta_loss="kullback-leibler",
                                                               solver="mu",
-                                                              max_iter=400)
+                                                              max_iter=max_iter)
+    return components, weights, n_iters
+
+
+def learn_cqt_representation(audio: np.ndarray, n_components: int = 100, max_iter: int = 400):
+    mags, phases = get_cqt_magphase(audio)
+    components, weights, n_iters = non_negative_factorization(mags,
+                                                              n_components=n_components,
+                                                              beta_loss="kullback-leibler",
+                                                              solver="mu",
+                                                              max_iter=max_iter)
+
     return components, weights, n_iters
 
 
--- nmf.py
+++ nmf.py
@@ -4,10 +4,11 @@ from pathlib import Path
 import numpy as np
 import musdb
 # import matplotlib.pyplot as plt
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import orthogonal_mp
 from soundfile import write
-import helpers
 from multiprocessing import Pool
+
+from helpers import NMFResults, generate_four_stem_data, get_magphase, learn_representation, make_mono, reconstruct_audio
 
 
 # TODO: MAKE EVERYTHING A FUNCTION
@@ -21,66 +22,106 @@ if __name__ == "__main__":
                          split="valid", is_wav=True)
     mus_test = musdb.DB(root=root, subsets="test", is_wav=True)
 
-    mixture, vocals, accompaniment, rate = next(
-        helpers.generate_two_stem_data(mus_train, chunk_duration=5.0))
+    mixture, vocals, drums, bass, others, rate = next(
+        generate_four_stem_data(mus_train, chunk_duration=10.0))
 
-    vocals_mono = helpers.make_mono(vocals)
-    accompaniment_mono = helpers.make_mono(accompaniment)
+    vocals_mono = make_mono(vocals)
+    drums_mono = make_mono(drums)
+    bass_mono = make_mono(bass)
+    others_mono = make_mono(others)
 
-    # Hyperparameter
-    n_components = 40
+    # Hyperparameters
+    win_length = 8192
+    n_components = 30
 
     pool = Pool()
 
-    results = helpers.NMFResults()
+    results = NMFResults()
 
     print("Starting Vocals")
-    pool.apply_async(helpers.learn_representation,
+    pool.apply_async(learn_representation,
                      args=(vocals_mono,),
                      kwds={
+                         "win_length": win_length,
                          "n_components": n_components,
-                         "max_iter": 400
+                         "max_iter": 600
                      },
                      callback=results.set_vocals)
 
-    print("Starting Accompaniment")
-    pool.apply_async(helpers.learn_representation,
-                     args=(accompaniment_mono,),
+    print("Starting Drums")
+    pool.apply_async(learn_representation,
+                     args=(drums_mono,),
                      kwds={
+                         "win_length": win_length,
                          "n_components": n_components,
-                         "max_iter": 400
+                         "max_iter": 600
                      },
-                     callback=results.set_accompaniment)
+                     callback=results.set_drums)
+
+    print("Starting Bass")
+    pool.apply_async(learn_representation,
+                     args=(bass_mono,),
+                     kwds={
+                         "win_length": win_length,
+                         "n_components": n_components,
+                         "max_iter": 600
+                     },
+                     callback=results.set_bass)
+
+    print("Starting Others")
+    pool.apply_async(learn_representation,
+                     args=(others_mono,),
+                     kwds={
+                         "win_length": win_length,
+                         "n_components": n_components,
+                         "max_iter": 600
+                     },
+                     callback=results.set_others)
 
     pool.close()
     pool.join()
 
     vocals_components, vocals_weights, vocals_iter = results.vocals
-    accompaniment_components, accompaniment_weights, accompaniment_iter = results.accompaniment
+    drums_components, drums_weights, drums_iter = results.drums
+    bass_components, bass_weights, bass_iter = results.bass
+    others_components, others_weights, others_iter = results.others
 
     mixture_components = np.hstack(
-        (vocals_components, accompaniment_components))
+        (vocals_components, drums_components, bass_components, others_components))
 
-    mixture_mono = helpers.make_mono(mixture)
-    mixture_mags, mixture_phases = helpers.get_magphase(mixture_mono)
+    mixture_mono = make_mono(mixture)
+    mixture_mags, mixture_phases = get_magphase(
+        mixture_mono, win_length=win_length)
 
-    # TODO: Replace with KL Divergence based iterations
-    print("Starting Separation with Lasso")
-    model = Lasso(alpha=0.1, fit_intercept=False,
-                  positive=True, selection="random")
-    model.fit(mixture_components, mixture_mags)
-    mixture_weights = model.coef_.T
-    print("Finished Separation with Lasso")
+    print("Starting Separation with OMP")
+    mixture_weights = orthogonal_mp(mixture_components,
+                                    mixture_mags,
+                                    n_nonzero_coefs=(4 * n_components // 3))
+    print("Finished Separation with OMP")
 
     learned_vocals_weights = mixture_weights[:n_components, :]
-    learned_accompaniment_weights = mixture_weights[n_components:, :]
+    learned_drums_weights = mixture_weights[n_components:(2 * n_components), :]
+    learned_bass_weights = mixture_weights[(
+        2 * n_components):(3 * n_components), :]
+    learned_others_weights = mixture_weights[(3 * n_components):, :]
 
-    learned_vocals = helpers.reconstruct_audio(
-        vocals_components, learned_vocals_weights, mixture_phases)
-    learned_accompaniment = helpers.reconstruct_audio(
-        accompaniment_components, learned_accompaniment_weights, mixture_phases)
+    learned_vocals = reconstruct_audio(
+        vocals_components, learned_vocals_weights, mixture_phases, win_length=win_length)
+    learned_drums = reconstruct_audio(
+        drums_components, learned_drums_weights, mixture_phases, win_length=win_length)
+    learned_bass = reconstruct_audio(
+        bass_components, learned_bass_weights, mixture_phases, win_length=win_length)
+    learned_others = reconstruct_audio(
+        others_components, learned_others_weights, mixture_phases, win_length=win_length)
 
     write(results_path / "mixture.wav", mixture, rate)
+
+    write(results_path / "true_vocals.wav", vocals, rate)
+    write(results_path / "true_drums.wav", drums, rate)
+    write(results_path / "true_bass.wav", bass, rate)
+    write(results_path / "true_others.wav", others, rate)
+
     write(results_path / "learned_vocals.wav", learned_vocals, rate)
-    write(results_path / "learned_accompaniment.wav",
-          learned_accompaniment, rate)
+    write(results_path / "learned_drums.wav", learned_drums, rate)
+    write(results_path / "learned_bass.wav", learned_bass, rate)
+    write(results_path / "learned_others.wav", learned_others, rate)
