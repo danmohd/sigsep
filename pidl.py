Fixed divide by zero error during dictionary normalization

pidl.py
@@ -4,20 +4,22 @@ from pidl.helpers import generate_four_stem_data_batch, model_train, model_separ
 from os import mkdir
 from soundfile import write
 import joblib
+import numpy as np
 
 
 if __name__ == "__main__":
+    # np.seterr(all="raise")
     root = Path("..", "musdb18hq")
     results_path = Path(".", "results_pidl")
     evaldir = results_path / "eval"
 
     mus_train = musdb.DB(root=root, is_wav=True, subsets="train")
 
-    batch_size = 10
+    batch_size = 5
 
-    data_gen = generate_four_stem_data_batch(mus_train, batch_size=batch_size, chunk_duration_train=30.0, chunk_duration_test=15.0)
+    data_gen = generate_four_stem_data_batch(mus_train, batch_size=batch_size, chunk_duration_train=10.0, chunk_duration_test=15.0)
 
-    components, data = model_train(data_gen, win_length=512, n_components=30, batch_size=batch_size, n_iter=20)
+    components, data = model_train(data_gen, win_length=512, n_components=20, batch_size=batch_size, n_iter=20)
 
     data = next(data_gen)
 

pidl/helpers.py
@@ -152,8 +152,8 @@ def learn_dictionary(data: np.ndarray, n_components: int, n_iter: int):
     rng = np.random.default_rng()
     L = rng.uniform(low=-1, high=1, size=(Y.shape[0], n_components))
     L = 0.5 * (L + np.flipud(L))
-    Lnorm = np.linalg.norm(L, ord=2, axis=0)
-    L = L / Lnorm
+    # Lnorm = np.linalg.norm(L, ord=2, axis=0)
+    # L = L / Lnorm
 
     # Dimension of data (fft size) or Number of features
     N = Y.shape[0]
@@ -180,14 +180,13 @@ def learn_dictionary(data: np.ndarray, n_components: int, n_iter: int):
                 (D, np.fft.ifft(Lambda[:, np.newaxis] * F, axis=0, norm="ortho")))
         D = np.real(D)
 
-        # D_norm = np.linalg.norm(D, ord=2, axis=0)
-        # D = D / D_norm
+        D, D_norm = normalize(D, norm="l2", axis=0, return_norm=True)
 
         # Sparse Update
         print(f"Iteration: {it}, Sparse Update")
         model.fit(D, Y)
         X = model.coef_.T
-        # X = X * D_norm[:, np.newaxis]
+        X = X * D_norm[:, np.newaxis]
 
         # Dictionary update
         # Do Blockwise update
@@ -197,7 +196,10 @@ def learn_dictionary(data: np.ndarray, n_components: int, n_iter: int):
             Zl = np.fft.fft(Y, axis=0, norm="ortho")
             for otherblock in range(n_components):
                 if otherblock != block:
-                    Zl -= np.diag(L[:, otherblock]) @ np.fft.fft(
+                    Ls = L[:, otherblock]
+                    # Zl -= np.diag(L[:, otherblock]) @ np.fft.fft(
+                    #     X[N * otherblock: N * (otherblock + 1), :], axis=0, norm="ortho")
+                    Zl -= Ls[:, np.newaxis] * np.fft.fft(
                         X[N * otherblock: N * (otherblock + 1), :], axis=0, norm="ortho")
 
             # Do lambda-wise update for each row in Zl
@@ -208,8 +210,8 @@ def learn_dictionary(data: np.ndarray, n_components: int, n_iter: int):
                 Zli = Zl[row, :]
                 L[row, block] = Zli @ FXli / \
                     (1e-16 + np.linalg.norm(FXli) ** 2)
-        Lnorm = np.linalg.norm(L, ord=2, axis=0)
-        L = L / Lnorm
+        # Lnorm = np.linalg.norm(L, ord=2, axis=0)
+        # L = L / Lnorm
 
     Lambda = L[:, 0]
     D = np.fft.ifft(Lambda[:, np.newaxis] * F, axis=0, norm="ortho")
@@ -218,7 +220,7 @@ def learn_dictionary(data: np.ndarray, n_components: int, n_iter: int):
         D = np.hstack(
             (D, np.fft.ifft(Lambda[:, np.newaxis] * F, axis=0, norm="ortho")))
     D = np.real(D)
-    # D = normalize(D, norm="l2", axis=0)
+    D = normalize(D, norm="l2", axis=0)
 
     return D
 